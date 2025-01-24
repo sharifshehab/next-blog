8e3c283b03ab3b8ad7d10919024e9329f7b8cf71
Shrafi Shehab <svshuvo696@gmail.com>
2025-01-24 16:22:01 +0600

    favicon changed

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
     return (
         <div className='container mx-auto min-h-screen flex flex-col items-center justify-center'>
             <div>
-                <h1>Welcome to your profile!</h1>
+                <h1 className='text-3lx text-white font-semibold'>Welcome to your profile!</h1>
            </div>
         </div>
     );
